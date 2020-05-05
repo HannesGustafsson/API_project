dd5f0102359db0fa3d8b21cf23ed3c588b0021d1
Hannes Gustafsson <hannesgustafsson@hotmail.com>
2020-05-05 15:47:47 +0200

    added forecast api

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const uri = 'https://5ea6a04c84f6290016ba6f29.mockapi.io/api/v1/visitors'
+const uriVisitors = 'https://5ea6a04c84f6290016ba6f29.mockapi.io/api/v1/visitors'
 
 // LOADS DATA
 function loadData() {
     const datadiv = document.getElementById('datadiv');
     datadiv.innerHTML = '';
-    fetch(uri)
+    fetch(uriVisitors)
         .then((resp) => resp.json())
         .then(function (data) {
             return data.map(function (data) {
@@ -22,7 +22,7 @@ function addVisitor() {
     const textInput = document.getElementById('name').value;
     var data = { name: textInput };
 
-    fetch(uri,
+    fetch(uriVisitors,
         {
             method: "POST",
             mode: "cors",
@@ -46,7 +46,7 @@ function addVisitor() {
 // DELETE VISITOR
 function deleteVisitor(id) {
 
-    fetch(uri + "/" + id,
+    fetch(uriVisitors + "/" + id,
         {
             method: "DELETE",
             mode: "cors",
@@ -63,4 +63,46 @@ function deleteVisitor(id) {
         .then(res => console.log(res))
 
         setTimeout(() => {loadData()}, 500);                        
+}
+
+// LOADS UPCOMING TRAIN DEPARTURES
+function getTrainTimes() {
+    const departurediv = document.getElementById('departures');
+
+    const uriTrains = 'https://cors-anywhere.herokuapp.com/http://api.sl.se/api2/realtimedeparturesV4.json?key=81a6a09603e14d51a73276f98ba095bb&siteid=9297&timewindow=10';
+    fetch(uriTrains)
+        .then((resp) => resp.json())
+        .then(function (data) {
+            console.log(data)
+            let departures = data.ResponseData.Metros;
+            return departures.map(function (departure) {
+                let divtag = document.createElement('div');
+
+                divtag.innerHTML = `${departure.LineNumber + " " + departure.Destination + " " + departure.DisplayTime}`;
+                departurediv.appendChild(divtag);
+            })
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+function getForecast() {
+    const divForecast = document.getElementById('divForecast');
+    const uriForecast = 'https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.0629/lat/59.3182/data.json';
+
+    fetch(uriForecast)
+        .then((resp) => resp.json())
+        .then(function (data) {
+            console.log(data)
+            let forecasts = data.timeSeries[0].parameters;
+            let divtag = document.createElement('div');
+
+            divtag.innerHTML = `${'Temperatur: ' + forecasts[1].values[0]}`;
+            divForecast.appendChild(divtag);
+
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
